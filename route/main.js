b4e7efc5767f587e83ae453d50c5430f05217278
KJY <rntlls1366@gmail.com>
2023-08-14 15:04:13 +0900

    error alert 개선

--- route/main.js
+++ route/main.js
@@ -39,7 +39,7 @@ router.post('/ask', async (req, res) => {       //ask url로 json형식 요청�
         ipMap.set(ip, count + 1);
     }
 
-    if (count < 50 || count === undefined) {
+    if (count < 30 || count === undefined) {
         const prompt = req.body.prompt;
         const response = await callChatGPT(prompt);
 
